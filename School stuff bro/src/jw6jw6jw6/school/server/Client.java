4757c951e724b72a9ebdc4bb35f366e7faae6040
jw6jw6jw6 <jw6jw6jw6@gmail.com>
2013-05-14 12:16:07 -0500

    Server should be fully functional

--- School stuff bro/src/jw6jw6jw6/school/server/Client.java
+++ School stuff bro/src/jw6jw6jw6/school/server/Client.java
@@ -15,16 +15,28 @@ public class Client
       {
          System.out.println("Connecting to " + serverName + " on port " + port);
          Socket client = new Socket(serverName, port);
-         System.out.println("Just connected to " + client.getRemoteSocketAddress());
+         System.out.println("Connected to " + client.getRemoteSocketAddress());
          OutputStream outToServer = client.getOutputStream();
          DataOutputStream out = new DataOutputStream(outToServer);
          InputStream inFromServer = client.getInputStream();
          DataInputStream in = new DataInputStream(inFromServer);
          System.out.println(in.readUTF());
-         System.out.println("Enter user");
-         out.writeUTF(input.nextLine());
-         System.out.println(in.readUTF());
-         System.out.println(in.readUTF());
+         String temp = input.nextLine();
+         out.writeUTF(temp);
+         if(Integer.parseInt(temp) == 1)
+         {
+        	 System.out.println(in.readUTF());
+        	 out.writeUTF(input.nextLine());
+        	 System.out.println(in.readUTF());
+        	 System.out.println(in.readUTF());
+         }
+         else
+         {
+//        	 System.err.println("ELSE");
+        	 System.out.println(in.readUTF());
+        	 out.writeUTF(input.nextLine());
+        	 System.out.println(in.readUTF());
+         }
          client.close();
       }catch(IOException e)
       {
--- School stuff bro/src/jw6jw6jw6/school/server/MoneyServer.java
+++ School stuff bro/src/jw6jw6jw6/school/server/MoneyServer.java
@@ -41,7 +41,7 @@ public class MoneyServer extends Thread
          {
             System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
             java.net.Socket server = serverSocket.accept();
-            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+            System.out.println("[CONNECTED]" + server.getRemoteSocketAddress());
             DataOutputStream out = new DataOutputStream(server.getOutputStream());
             out.writeUTF("Thank you for connecting to the Bank of good fortune, Do you have an account? (1 for yes, 2 for no)");
             DataInputStream in = new DataInputStream(server.getInputStream());
@@ -49,21 +49,35 @@ public class MoneyServer extends Thread
             Double forwards = 0.0;
             if(Integer.parseInt(tmp) == 1)
             {
+            	out.writeUTF("Enter user ID");
             	String user = in.readUTF();
+            	if(Money.containsKey(Integer.parseInt(user)))
+            	{
             	forwards = Money.get(Integer.parseInt(user));
             	out.writeUTF("You have "+forwards+" Forwards");
+            	out.writeUTF("Would you like to transfer Forwards?");
+            	tmp = in.readUTF();
+            	if(Integer.parseInt(tmp) == 1)
+            	{
+            		out.writeUTF("Enter ID to transfer Forwards to");
+            		transfer(Integer.parseInt(user),Integer.parseInt(in.readUTF()),Double.parseDouble(in.readUTF()));
+            	}
+            	}
+            	else
+            		out.writeUTF("User ID not found!");
             }
             else
             {
-            	System.out.println("Would you like to open an account?");
+            	out.writeUTF("Would you like to open an account?");
             	tmp = in.readUTF();
+            	System.out.println(tmp);
             	if(Integer.parseInt(tmp) == 1)
             		Money.put(Money.size(), 0.0);
-            	int tmp1 = Money.size() - 1;
+            	int tmp1 = Money.size();
             	out.writeUTF("Your Account has been created, your user ID is " + tmp1);
             }
             out.writeUTF("Ending Connection");
-            server.close();
+//            server.close();
          }catch(IOException e)
          {
             e.printStackTrace();
